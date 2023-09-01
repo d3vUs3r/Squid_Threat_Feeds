26b6fd56884d9ca39364b1baf15deccb247a79a1
d3vUs3r <115107997+d3vUs3r@users.noreply.github.com>
2023-09-01 01:08:10 +0100

    Update squidGuardian.sh
    fix

--- squidGuardian.sh
+++ squidGuardian.sh
@@ -2,26 +2,37 @@
 
 # Function to update Squid bad URL list
 update_squid_bad_urls() {
-    echo "$(date +\%Y-\%m-\%d\ %H:\%M:\%S) Updating squid bad URLs..." >> /path/to/your/logfile.log
-    local output_file="urlhaus_data.csv"
+    echo "$(date +\%Y-\%m-\%d\ %H:\%M:\%S) Updating squid bad URLs..." >> logs/logfile.log
+    local output_file="data/urlhaus_data.csv"
     local extracted_urls_file="extracted_urls.txt"
     local squid_bad_url_file="/etc/squid/bad_url.txt"
     local archive_folder="archive/"
+    local temp_file="data/temp.csv"
 
 
     # Calculate the hash of the previously downloaded CSV data
     local previous_hash=$(sha256sum "$output_file" 2>/dev/null | cut -d ' ' -f 1)
     
     # Use curl to download the CSV data
-    curl -o "$output_file" "https://urlhaus.abuse.ch/downloads/csv_online/"
+    curl -o "$temp_file" "https://urlhaus.abuse.ch/downloads/csv_online/"
+
     
     # Calculate the hash of the newly downloaded CSV data
-    local new_hash=$(sha256sum "$output_file" | cut -d ' ' -f 1)
+    local new_hash=$(sha256sum "$temp_file" | cut -d ' ' -f 1)
     
     # Check if the download was successful and if the data has changed
     if [ $? -eq 0 ]; then
         if [ "$new_hash" != "$previous_hash" ]; then
             echo "Data has changed. Extracting URLs and saving to $extracted_urls_file."
+
+            # Move the previous output file to the archive folder and rename with date and time
+            if [ -f "$output_file" ]; then
+                archive_filename="$(basename "$output_file").$(date +\%Y-\%m-\%d_\%H-\%M-\%S)"
+                mv "$output_file" "$archive_folder/$archive_filename"
+                echo "Previous file moved to $archive_folder/$archive_filename."
+            fi
+
+            mv "$temp_file" "$output_file"
     
             # Extract URLs and save them to the extracted URLs file
             tail -n +12 "$output_file" | cut -d '"' -f 6 > "$extracted_urls_file"
@@ -29,12 +40,7 @@ update_squid_bad_urls() {
             echo "Extracted URLs saved to $extracted_urls_file."
             
             # new part added to test
-            # Move the previous output file to the archive folder and rename with date and time
-            if [ -f "$output_file" ]; then
-                archive_filename="$(basename "$output_file").$(date +\%Y-\%m-\%d_\%H-\%M-\%S)"
-                mv "$output_file" "$archive_folder/$archive_filename"
-                echo "Previous file moved to $archive_folder/$archive_filename."
-            fi
+
 
             # Replace content within the Squid bad URL section
             awk '/# BeginAbuseFeedBadURLs/ { print; p=1 } p && /# EndbuseFeedBadURLs/ { system("cat '"$extracted_urls_file"'"); p=0 } !p' "$squid_bad_url_file" > temp_file
@@ -43,6 +49,7 @@ update_squid_bad_urls() {
             echo "Extracted URLs added to the Squid bad URL section in $squid_bad_url_file."
         else
             echo "Data has not changed since the last check."
+            rm -f "$temp_file"
         fi
     else
         echo "Error downloading the data."
@@ -53,26 +60,38 @@ update_squid_bad_urls() {
 
 # Function to update bad IP addresses from Feodotracker feed
 update_IpFeed_Feodotracker() {
-    echo "$(date +\%Y-\%m-\%d\ %H:\%M:\%S) Updating IpFeed Feodotracker..." >> /path/to/your/logfile.log	
-    local output_file="bad_ips_data.txt"
+    echo "$(date +\%Y-\%m-\%d\ %H:\%M:\%S) Updating IpFeed Feodotracker..." >> /logs/logfile.log	
+    local output_file="data/bad_ips_data.txt"
     local extracted_ips_file="extracted_ips.txt"
     local config_file="/etc/squid/bad_ips.txt"
+    local archive_folder="archive/"
+    local temp_file="data/temp.txt"
 
 
     # Calculate the hash of the previously downloaded text data
     local previous_hash=$(sha256sum "$output_file" 2>/dev/null | cut -d ' ' -f 1)
 
     # Use curl to download the text data
-    curl -o "$output_file" "https://feodotracker.abuse.ch/downloads/ipblocklist.txt"
+    curl -o "$temp_file" "https://feodotracker.abuse.ch/downloads/ipblocklist.txt"
+
 
     # Calculate the hash of the newly downloaded text data
-    local new_hash=$(sha256sum "$output_file" | cut -d ' ' -f 1)
+    local new_hash=$(sha256sum "$temp_file" | cut -d ' ' -f 1)
 
     # Check if the download was successful and if the data has changed
     if [ $? -eq 0 ]; then
         if [ "$new_hash" != "$previous_hash" ]; then
             echo "Data has changed. Extracting IPs and saving to $extracted_ips_file."
-            
+
+            # Move the previous output file to the archive folder and rename with date and time
+            if [ -f "$output_file" ]; then
+                archive_filename="$(basename "$output_file").$(date +\%Y-\%m-\%d_\%H-\%M-\%S)"
+                mv "$output_file" "$archive_folder/$archive_filename"
+                echo "Previous file moved to $archive_folder/$archive_filename."
+            fi
+
+            mv "$temp_file" "$output_file"
+
             # Extract IPs and save them to the extracted IPs file
             grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' "$output_file" > "$extracted_ips_file"
             
@@ -92,11 +111,12 @@ update_IpFeed_Feodotracker() {
     }
     { print }
     ' "$config_file" > temp_config_file
-	     mv temp_config_file "$config_file"
-	     rm "$extracted_ips_file"
-            echo "Extracted IP addresses are saved in $extracted_ips_file"
+	        mv temp_config_file "$config_file"
+	        rm "$extracted_ips_file"
+           echo "Extracted IP addresses are saved in $extracted_ips_file"
         else
             echo "Data has not changed since the last check."
+            rm -f "$temp_file"
         fi
     else
         echo "Error downloading the data."
@@ -253,4 +273,5 @@ case "$1" in
         exit 1
         ;;
 esac
+
 
